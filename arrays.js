07aeb3e874023a54d4df9489ab5cdcb6454587bd
Senai Mogos <smogos797@gmail.com>
2019-01-18 03:48:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,47 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(array, element) {
+     ["Orange", ...addElementToBeginningOfArray]
+}
+
+ function destructivelyAddElementToBeginningOfArray(array, element) {
+    destructivelyAddElementToBeginningOfArray.unshift("Meron")
+  }
+
+
+function addElementToEndOfArray(array, element){
+    [...dessert, "Cake"]
+    return addElementToEndOfArray
+  }
+
+function destructivelyAddElementToEndOfArray(array, element) {
+    destructivelyAddElementToEndOfArray.push(91)
+  }
+
+function accessElementInArray(name, 1) {
+    name = ["Megan", "Mike", "Maria"]
+    console.log(name[1]);
+  }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+    destructivelyRemoveElementFromBeginningOfArray.shift()
+  }
+
+function removeElementFromBeginningOfArray(array) {
+    var time = ["morning", "noon", "night"]
+
+    time.shift(0)
+
+    time
+  }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+    var lunch = ["hamburger", "pizza", "salad"]
+    lunch.pop()
+    lunch
+  }
+
+function removeElementFromEndOfArray(array) {
+    flavor = ["vanilla", "chocolate", "strawberry"]
+    flavor.slice(0, flavor.length - 1)
+
+  }
