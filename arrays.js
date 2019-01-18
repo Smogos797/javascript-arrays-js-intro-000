d37152a2e362a050972da1cb3e53e3c80cd7b35b
Senai Mogos <smogos797@gmail.com>
2019-01-18 04:49:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,17 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
- addElementToBeginningOfArray = ["Lime", "pine"]
- function addElementToBeginningOfArray(array, element)
+
+function addElementToBeginningOfArray(array, element) {
+  array = ["Meron", "Ariam"]
+  ["Senai", ...array]
+}
+
+function happyHolidaysTo(name) {
+  return ("Happy holidays, you!")
+}
+
+function happyHolidayTo(holiday, name) {
+  return ("Happy Independence Day, you!")
+}
+
+function holidayCountdown(days, holiday) {
+  return ("It\'s 20 days until Mother\'s Day!")
+}
